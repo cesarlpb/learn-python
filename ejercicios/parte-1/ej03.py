6cfbbadfd13223fd283e37416ffa3ea14bf47930
César <cesarlpb@gmail.com>
2024-06-06 19:25:21 +0200

    Ej03 código inicial

--- ejercicios/parte-1/ej03.py
+++ ejercicios/parte-1/ej03.py
@@ -2,3 +2,13 @@
 """
 3 - Definir una función que calcule la longitud de una lista o una cadena dada. (Es cierto que python tiene la función len() incorporada, pero escribirla por nosotros mismos resulta un muy buen ejercicio.
 """
+# %% built-in len:
+string = "Hola"
+print(len(string))
+
+# %% bucle
+def contar_chars(string: str) -> int:
+  pass # -> return
+#%% Tests
+string = "Hola, qué tal?" # 14
+assert contar_chars(string) == 14
