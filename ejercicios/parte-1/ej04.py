59068d9e710fb65bf523fcd4d4dfbc6b0e480ad7
César <cesarlpb@gmail.com>
2024-06-06 19:32:15 +0200

    Ej04 código inicial

--- ejercicios/parte-1/ej04.py
+++ ejercicios/parte-1/ej04.py
@@ -2,3 +2,14 @@
 """
 4 - Escribir una función que tome un carácter y devuelva True si es una vocal, de lo contrario devuelve False.
 """
+#%% Verificar si el elemento está en una lista:
+frutas = ["manzana", "pera", "kiwi"]
+fruta_random = "kiwi"
+print(fruta_random in frutas)
+#%% Ejercicio:
+# vocales_lista = [...]
+def verificar_vocal(vocal: str) -> bool:
+  pass
+#%% Test
+assert verificar_vocal('a') == True
+assert verificar_vocal('z') == False
