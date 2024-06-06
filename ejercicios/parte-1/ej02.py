5c5ef2bb7c3bc525887129c0797c013a45443d24
César <cesarlpb@gmail.com>
2024-06-06 19:14:51 +0200

    Ej 02 código inicial

--- ejercicios/parte-1/ej02.py
+++ ejercicios/parte-1/ej02.py
@@ -2,3 +2,20 @@
 """
   2 - Definir una función max_de_tres(), que tome tres números como argumentos y devuelva el mayor de ellos.
 """
+
+# %% Extensión de la función calcular_maximo_dos del ej01:
+def calcular_maximo_tres(a, b, c):
+  pass # escribe aquí la solución
+
+# %% Tests
+r_1 = calcular_maximo_tres(1, 2, 3) # 3
+assert r_1 == 3
+
+# %% ¿Cómo se puede calcular el máximo o mínimo de una lista de números?
+# Por simplicidad podemos suponer que son int
+def calcular_maximo_lista(lista: list):
+  pass # escribe aquí la solución
+# %% Tests
+lista = [-1, 0, 1] # max -> 1
+res = calcular_maximo_lista(lista)
+assert res == 1
