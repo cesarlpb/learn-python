bd28256d55b08e6c019411565e3859fa77bad0bd
César <cesarlpb@gmail.com>
2024-06-06 19:21:18 +0200

    bucle if elif else

---
+++ intro/if-elif-else.py
@@ -0,0 +1,9 @@
+# %% Bucle if elif else:
+edad = int(input("Introduce tu edad"))
+if edad > 18:
+  print("Eres mayor de edad")
+elif edad == 18:
+  print("Tienes justo 18!")
+else: 
+  print("Eres menor de edad")
+# %%
