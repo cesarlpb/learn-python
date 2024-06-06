6f4010cf92de039b3c46eac0b2746fb12f223bea
César <cesarlpb@gmail.com>
2024-06-06 19:37:50 +0200

    Ejercicio 05 código inicial

--- ejercicios/parte-1/ej05.py
+++ ejercicios/parte-1/ej05.py
@@ -1,4 +1,17 @@
 # https://pythondiario.com/2013/05/ejercicios-en-python-parte-1.html
 """
-  5 - Escribir una función sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista. Por ejemplo: sum([1,2,3,4]) debería devolver 10 y multip([1,2,3,4]) debería devolver 24.
+  5 - Escribir una función sum() y una función multip() que sumen y multipliquen respectivamente todos los números de una lista. 
+  
+  Por ejemplo: 
+  - suma([1,2,3,4]) debería devolver 10 
+  - y multiplicacion([1,2,3,4]) debería devolver 24.
 """
+#%% suma
+def suma(lista: list) -> int | float:
+  pass
+#%% multiplicacion
+def multiplicacion(lista: list) -> int | float:
+  pass
+#%% Tests
+assert suma([1,2,3,4]) == 10
+assert multiplicacion([1,2,3,4]) == 24
