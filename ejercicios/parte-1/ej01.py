d7fceb81b0424ac2b6be6bac47431653484ec2bc
César <cesarlpb@gmail.com>
2024-06-06 18:15:39 +0200

    Cómo crear celdas con Jupyter

---
+++ ejercicios/parte-1/ej01.py
@@ -0,0 +1,18 @@
+# https://pythondiario.com/2013/05/ejercicios-en-python-parte-1.html
+
+"""
+  1 - Definir una función max() que tome como argumento dos números y devuelva el mayor de ellos. (Es cierto que python tiene una función max() incorporada, pero hacerla nosotros mismos es un muy buen ejercicio.
+"""
+
+# %% Esto es una celda
+print("Hola")
+
+# %% Esto es otra celda
+print(1+1)
+print("2")
+
+# %% Empezamos a resolver el ejercicio aquí:
+
+
+
+
