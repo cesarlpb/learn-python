0941b471c3e5a25da1cb5f9486b0a9417095d7ed
César <cesarlpb@gmail.com>
2024-07-05 12:02:27 +0200

    Añado main()

--- libs/ejercicios/ejercicio-2.py
+++ libs/ejercicios/ejercicio-2.py
@@ -3,32 +3,36 @@
 import pandas as pd
 import matplotlib.pyplot as plt
 
-# Cargar el dataset
-data = pd.read_csv('data.csv')
+def main():
+  # Cargar el dataset
+  data = pd.read_csv('data.csv')
 
-# Histograma de alturas
-plt.figure(figsize=(10, 6))
-plt.hist(data['height'], bins=30, edgecolor='black')
-plt.title('Histograma de Alturas')
-plt.xlabel('Altura (cm)')
-plt.ylabel('Frecuencia')
-plt.show()
+  # Histograma de alturas
+  plt.figure(figsize=(10, 6))
+  plt.hist(data['height'], bins=30, edgecolor='black')
+  plt.title('Histograma de Alturas')
+  plt.xlabel('Altura (cm)')
+  plt.ylabel('Frecuencia')
+  plt.show()
 
-# Diagrama de dispersión de pesos vs edades
-plt.figure(figsize=(10, 6))
-plt.scatter(data['age'], data['weight'], alpha=0.5)
-plt.title('Diagrama de Dispersión de Pesos vs Edades')
-plt.xlabel('Edad')
-plt.ylabel('Peso (kg)')
-plt.show()
+  # Diagrama de dispersión de pesos vs edades
+  plt.figure(figsize=(10, 6))
+  plt.scatter(data['age'], data['weight'], alpha=0.5)
+  plt.title('Diagrama de Dispersión de Pesos vs Edades')
+  plt.xlabel('Edad')
+  plt.ylabel('Peso (kg)')
+  plt.show()
 
-# Gráfica de barras de promedios de altura por género
-height_means = data.groupby('gender')['height'].mean()
+  # Gráfica de barras de promedios de altura por género
+  height_means = data.groupby('gender')['height'].mean()
 
-plt.figure(figsize=(10, 6))
-height_means.plot(kind='bar', color=['blue', 'orange'])
-plt.title('Promedio de Altura por Género')
-plt.xlabel('Género')
-plt.ylabel('Altura Promedio (cm)')
-plt.xticks(rotation=0)
-plt.show()
+  plt.figure(figsize=(10, 6))
+  height_means.plot(kind='bar', color=['blue', 'orange'])
+  plt.title('Promedio de Altura por Género')
+  plt.xlabel('Género')
+  plt.ylabel('Altura Promedio (cm)')
+  plt.xticks(rotation=0)
+  plt.show()
+
+if __name__ == '__main__':
+  main()
